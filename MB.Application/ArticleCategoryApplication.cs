f1d61408a90d768566a65daf52aa76e77d1ed363
PZiamanesh <po.ziamanesh@gmail.com>
2022-10-24 09:27:43 +0330

    Add project files.

---
+++ MB.Application/ArticleCategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿using MB.Application.Contracts.ArticleCategory;
+
+namespace MB.Application
+{
+    public class ArticleCategoryApplication : IArticleCategoryApplication
+    {
+        public List<ArticleCategoryViewModel> List()
+        {
+            // toDo: get list of Domain model (map)
+            // toDo: return to ui
+        }
+    }
+}
