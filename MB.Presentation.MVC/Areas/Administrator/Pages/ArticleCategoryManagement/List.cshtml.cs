de5c7f3b951e350f05bd20ea42f950f66b074be5
PZiamanesh <po.ziamanesh@gmail.com>
2022-10-24 13:31:27 +0330

    master_ Administration UI added.

---
+++ MB.Presentation.MVC/Areas/Administrator/Pages/ArticleCategoryManagement/List.cshtml.cs
@@ -0,0 +1,20 @@
+using MB.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MB.Presentation.MVC.Areas.Administrator.Pages.ArticleCategoryManagement;
+
+public class ListModel : PageModel
+{
+    private readonly IArticleCategoryApplication _articleCategoryApplication;
+    public List<ArticleCategoryViewModel> ArticleCategories { get; set; }
+
+    public ListModel(IArticleCategoryApplication articleCategoryApplication)
+    {
+        _articleCategoryApplication = articleCategoryApplication;
+    }
+
+    public void OnGet()
+    {
+        ArticleCategories = _articleCategoryApplication.List();
+    }
+}
