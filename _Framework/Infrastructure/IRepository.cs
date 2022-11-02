cbf48f110ff786548b564d812c0ae50d9426b461
PZiamanesh <po.ziamanesh@gmail.com>
2022-11-02 11:25:58 +0330

    master_ frameword structure added

---
+++ _Framework/Infrastructure/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using _Framework.Domain;
+
+namespace _Framework.Infrastructure;
+
+public interface IRepository<in TKey, TEntity> where TEntity : DomainBase<TKey>
+{
+    void Create(TEntity entity);
+    IEnumerable<TEntity> GetAll();
+    TEntity Get(TKey key);
+    bool Exist(Expression<Func<TEntity, bool>> expression);
+}
